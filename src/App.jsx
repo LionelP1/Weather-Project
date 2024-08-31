4675916d81ceca913302327384b5b6a0ea7b18e6
Lionel <lpei1900@gmail.com>
2024-08-31 12:32:49 -0400

    Finished implementing the data fetching functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
-import { fetchWeather, fetch7DayForecast, fetchHourlyForecastNext5Hours } from './api';
+import { fetchWeatherWithCity, fetchWeatherWithCoords, LocationInfo } from './api';
 
 function App() {
-  console.log(fetchWeather('Toronto'));
-  console.log(fetch7DayForecast(43.7,79.42));
-  console.log(fetchHourlyForecastNext5Hours(43.7,79.42));
  return(
     <h1>Hello</h1>
  );
--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 const API_KEY = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
 const URL = 'https://api.openweathermap.org/data/2.5';
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 // Fetch current weather
-const fetchWeather = async (city) => {
+const fetchWeatherWithCity = async (city) => {
   try {
-    const url = `${URL}/weather?q=${encodeURIComponent(city)}&appid=${API_KEY}`;
+    const url = `${URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -19,5 +20,55 @@ const fetchWeather = async (city) => {
   }
 };
 
+const fetchWeatherWithCoords = async (lat,lon) => {
+  try {
+    const url = `${URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const response = await fetch(url);
 
-export { fetchWeather };
+    if (!response.ok) {
+      throw new Error('Error fetching weather data');
+    }
+
+    return await response.json();
+
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    throw error;
+  }
+};
+
+const GEOCODING_URL = 'https://api.openweathermap.org/geo/1.0/direct';
+
+// LocationInfo function to get 5 possible cities and their location data
+// Returns array of location objects for the city
+const LocationInfo = async (city) => {
+  try {
+    const url = `${GEOCODING_URL}?q=${encodeURIComponent(city)}&limit=5&appid=${API_KEY}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Error fetching location data');
+    }
+
+    const locations = await response.json();
+
+    // Format all matches
+    const locationResults = locations.map(location => ({
+      name: location.name,
+      state: location.state,
+      country: location.country,
+      latitude: location.lat,
+      longitude: location.lon,
+    }));
+    
+    return locationResults;
+
+  } catch (error) {
+    console.error('Error fetching location data:', error);
+    throw error;
+  }
+};
+
+
+
+export { fetchWeatherWithCity, fetchWeatherWithCoords, LocationInfo };
